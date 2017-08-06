0be3e12e0687a25955ef8ee6799d1b1bdae76ea1
manelgavalda <expedientmanel@gmail.com>
2017-08-06 22:13:24 +0200

    Showing youtube data on console log

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
 
 const API_KEY = 'AIzaSyBK4PodglyUXKE2LfXeIRuKLTWLnbGDqhs';
+
+YTSearch( { key: API_KEY, term: 'surfboards' },  function(data) {
+  console.log(data);
+ } )
 
 // Create a new component. This component should produce some HTML.
 const App = () => {
