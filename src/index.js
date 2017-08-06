8ed045a6b5e7b2999ec196a242d0906334bd2e56
manelgavalda <expedientmanel@gmail.com>
2017-08-06 22:29:37 +0200

    Passing props from index to video_list

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
 const API_KEY = 'AIzaSyBK4PodglyUXKE2LfXeIRuKLTWLnbGDqhs';
 
 class App extends Component {
@@ -19,9 +20,10 @@ class App extends Component {
     return (
       <div>
         <SearchBar/>
+        <VideoList videos={ this.state.videos }/>
       </div>
     );
   }
-};
+}
 
 ReactDOM.render(<App/>, document.querySelector('.container'));
