359032d46971ee017f67f87b1ae070880ccc9c6c
manelgavalda <expedientmanel@gmail.com>
2017-08-06 22:21:29 +0200

    Added youtube search package and index refractor

--- src/index.js
+++ src/index.js
@@ -1,23 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
-
 import SearchBar from './components/search_bar';
-
 const API_KEY = 'AIzaSyBK4PodglyUXKE2LfXeIRuKLTWLnbGDqhs';
 
-YTSearch( { key: API_KEY, term: 'surfboards' },  function(data) {
-  console.log(data);
- } )
+class App extends Component {
+  constructor(props) {
+    super(props);
 
-// Create a new component. This component should produce some HTML.
-const App = () => {
-  return (
-    <div>
-      <SearchBar/>
-    </div>
-  );
+    this.state = { videos: [] };
+
+    YTSearch( { key: API_KEY, term: 'surfboards' }, videos => {
+      this.setState({ videos });
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar/>
+      </div>
+    );
+  }
 };
 
-// Take this component's generated HTML and put it on the page (DOM).
 ReactDOM.render(<App/>, document.querySelector('.container'));
