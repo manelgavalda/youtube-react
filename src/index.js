645fc17a2c228811b595b3686c57bbfe58334463
manelgavalda <expedientmanel@gmail.com>
2017-08-06 20:00:10 +0200

    Component with input on app

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 import React from 'react';
-import ReactDOM from 'react-dom'
+import ReactDOM from 'react-dom';
+
+import SearchBar from './components/search_bar';
+
+const API_KEY = 'AIzaSyBK4PodglyUXKE2LfXeIRuKLTWLnbGDqhs';
 
 // Create a new component. This component should produce some HTML.
 const App = () => {
-    return <div>Hi!</div>;
-}
+  return (
+    <div>
+      <SearchBar/>
+    </div>
+  );
+};
 
 // Take this component's generated HTML and put it on the page (DOM).
-ReactDOM.render(<App/>, document.querySelector('.container'))
+ReactDOM.render(<App/>, document.querySelector('.container'));
