1f05a171c2cc46614d388eb8cdb43ea114178fb5
manelgavalda <expedientmanel@gmail.com>
2017-08-06 23:01:29 +0200

    Added loading if there is no video

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 const API_KEY = 'AIzaSyBK4PodglyUXKE2LfXeIRuKLTWLnbGDqhs';
 
 class App extends Component {
@@ -11,7 +12,7 @@ class App extends Component {
 
     this.state = { videos: [] };
 
-    YTSearch( { key: API_KEY, term: 'surfboards' }, videos => {
+    YTSearch({key: API_KEY, term: 'surfboards'}, videos => {
       this.setState({ videos });
     });
   }
@@ -20,6 +21,7 @@ class App extends Component {
     return (
       <div>
         <SearchBar/>
+        <VideoDetail video={this.state.videos[0]}/>
         <VideoList videos={ this.state.videos }/>
       </div>
     );
