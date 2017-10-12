4eee73157ad6e3f2e25af4014061961c79881d03
manelgavalda <expedientmanel@gmail.com>
2017-10-12 12:43:43 +0200

    Added comment on serverjs

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const app = express();
 
 app.use(express.static(__dirname));
 
+// To make route still work on refresh
 app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, 'index.html'))
 });
